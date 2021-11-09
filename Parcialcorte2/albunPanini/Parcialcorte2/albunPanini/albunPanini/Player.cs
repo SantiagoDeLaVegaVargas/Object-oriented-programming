8eb5e1e3fbbff6b064c4453c44f647b6d40fb105
Santiago De La Vega Vargas <santiago.delavegavargas@gmail.com>
2021-11-09 11:28:13 -0500

    Add files via upload

---
+++ Parcialcorte2/albunPanini/Parcialcorte2/albunPanini/albunPanini/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace albunPanini
+{ 
+    enum Position
+        {
+        Forward,
+        Outfield,
+        OffensiveMidfield,
+        DefensiveMidfield,
+        Defense,
+        Goalkeeper,
+    }
+    class Player : Person
+    {
+
+        private int shoot;
+        private int defend;
+        private int speed;
+        private int power;
+        private Position position;
+
+
+        public Player(Position position, int shoot, int defend, int speed, int power, string firstName, string lastName, DateTime birthdayDate) : base(firstName, lastName, birthdayDate)
+        {
+            this.position = position;
+            this.shoot = shoot;
+            this.defend = defend;
+            this.speed = speed;
+            this.power = power;
+        }
+
+        public string longPass()
+        {
+            return "I am making a long pass";
+        }
+        public string shortPass()
+        {
+            return "I am making a short shot";
+        } 
+        public string shooting()
+        {
+            return "I am shooting";
+        }
+    }
+}
---
+++ Parcialcorte2/albunPanini/Parcialcorte2/albunPanini/albunPanini/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace albunPanini
+{
+    class Team
+    {
+        private string name;
+        private string description;
+        private DateTime foundationDate;
+        private Player[] players = new Player[11];
+
+
+        public Team(string name, string description, DateTime foundationDate)
+        {
+            this.name = name;
+            this.description = description;
+            this.foundationDate = foundationDate;
+            players = new Player(1,43,34,34,34,"jorgito","benavides", (2011, 6, 10));
+        }
+
+        private int getPositionVoid()
+        {
+            int i = 0;
+            for (i = 0; i < players.Length; i++)
+            {
+                if (players[i] == null)
+                    return i;
+            }
+            return i;
+        }
+
+
+
+    }
+}
