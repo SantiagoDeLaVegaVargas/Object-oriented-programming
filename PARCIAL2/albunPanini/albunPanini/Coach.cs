3f9330a072a627b16ba3533ae8903f043fee0b48
Santiago De La Vega Vargas <santiago.delavegavargas@gmail.com>
2021-11-09 11:29:20 -0500

    Add files via upload

---
+++ PARCIAL2/albunPanini/albunPanini/Coach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace albunPanini
+{
+    class Coach : Person
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+        private string trayectory;
+
+
+        public Coach(DateTime startDate, string firstName, string lastName, DateTime birthdayDate) : base(firstName, lastName, birthdayDate)
+        {
+            this.startDate = startDate;
+        }
+
+        public void getTime()
+        {
+
+        }
+    }
+}
---
+++ PARCIAL2/albunPanini/albunPanini/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace albunPanini
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int height;
+        private string path;
+        private DateTime birthdayDate;
+
+        public Person(string firstName, string lastName, DateTime birthdayDate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthdayDate = birthdayDate; 
+        }
+        
+        public int getAge()
+        {
+            int currentYear = DateTime.Now.Year;
+            int year = birthdayDate.Year;
+
+            return currentYear - year;
+        }
+
+        public string getFullName()
+        {
+            return "El nombre del jugador es :" + firstName + ' ' + LastName + ' ';
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+
+        public string Path
+        {
+            get => path;
+            set => path = value;
+        }
+
+        public DateTime BirthdayDate
+        {
+            get => birthdayDate;
+            set => birthdayDate = value;
+        }
+
+    }
+}
